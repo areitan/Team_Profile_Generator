325ec1e19dc5d8c09c6892dca0135b646eedb36e
April Reitan <45362322+areitan@users.noreply.github.com>
2022-04-30 00:51:04 -0700

    fixing package.json to add jest to testing script

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,10 +4,10 @@
 
 // getRole()—overridden to return 'Manager'
 
-const { default: TestRunner } = require("jest-runner");
 const manager = require("./lib/manager");
 
-test('array contains correct values', () => {
+test('array contains officeNumber', () => {
     const newManager = new manager(["joe", 1,"joe@joe.com","M4-A123"])
-    expect (newManager.officeNumber).toBe("M4-A123")
+    expect (manager.officeNumber).toEqual("M4-A123")
 });
+
